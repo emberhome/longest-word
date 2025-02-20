733d564635ec5ddbceade698816c3b254c7f5a09
Emma Berniczky <e.berniczky@gmail.com>
2025-02-20 19:31:06 +0000

    longest word tests

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,4 +1,4 @@
-
+import requests
 
 class Game:
     def __init__(self) -> list:
@@ -9,26 +9,44 @@ class Game:
         pass
 
 
-    def is_valid(self, word: str) -> bool:
-        """Return True if and only if the word is valid, given the Game's grid"""
+    # def is_valid(self, word: str) -> bool:
+    #     """Return True if and only if the word is valid, given the Game's grid"""
 
+    #     word_letters = list(word)
+
+    #     if len(word_letters) == 0:
+    #         return False
+
+    #     else:
+    #             grid_copy = self.grid.copy()
+
+    #             for l in word_letters:
+    #                if l in grid_copy: grid_copy.remove(l)
+    #                else:
+    #                    return False
+    #             return True
+
+
+    def is_valid(self, word):
         word_letters = list(word)
 
         if len(word_letters) == 0:
             return False
 
         else:
-                grid_copy = self.grid.copy()
-
-                for l in word_letters:
-                   if l in grid_copy: grid_copy.remove(l)
-                   else:
-                       return False
-                return True
+            return self.__check_dictionary(word)
 
 
-game = Game()
-print(game.grid) # --> OQUWRBAZE
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
 
-word = 'BAROQUE'
-print(game.is_valid(word)) # --> True
+
+
+# game = Game()
+# print(game.grid) # --> OQUWRBAZE
+
+# word = 'BAROQUE'
+# print(game.is_valid(word)) # --> True
